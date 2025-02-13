094a66e31a0883f30062034401eba65736d98d54
yerass11 <y_saiman@kbtu.kz>
2025-02-13 17:32:44 +0500

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Yerasyl on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
